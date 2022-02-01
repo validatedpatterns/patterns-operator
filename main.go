1aa8de1a3937d4d22de84edcbc3f74d1656a3069
Andrew Beekhof <andrew@beekhof.net>
2022-02-01 22:21:05 +1100

    Disable leader election for now

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 	var probeAddr string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", true,
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	opts := zap.Options{
@@ -72,7 +72,7 @@ func main() {
 		HealthProbeBindAddress:  probeAddr,
 		LeaderElection:          enableLeaderElection,
 		LeaderElectionID:        "f2850479.hybrid-cloud-patterns.io",
-		LeaderElectionNamespace: "default", // Use this if we ever want to enforce a single instance per cluster
+		//LeaderElectionNamespace: "default", // Use this if we ever want to enforce a single instance per cluster
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
