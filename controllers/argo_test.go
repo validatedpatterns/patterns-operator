a104202b2bf125acf2b5060406bb99d8976a81c5
Michele Baldessari <michele@acksyn.org>
2023-08-12 21:45:12 +0200

    Add tests for some of the functions dealing with Helm Parameters

--- controllers/argo_test.go
+++ controllers/argo_test.go
@@ -6,6 +6,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	argoapi "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 	api "github.com/hybrid-cloud-patterns/patterns-operator/api/v1alpha1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -85,6 +86,7 @@ var _ = Describe("Argo Pattern", func() {
 					"myprefix/test2.yaml")))
 			})
 		})
+
 		Context("With extra valuefiles with leading slashes", func() {
 			BeforeEach(func() {
 				pattern.Spec.ExtraValueFiles = []string{
@@ -104,6 +106,87 @@ var _ = Describe("Argo Pattern", func() {
 					"myprefix/test1.yaml",
 					"myprefix/test2.yaml")))
 			})
+		})
+	})
+
+	Describe("Argo Helm Functions", func() {
+		var goal, actual []string
+		var goalHelm, actualHelm []argoapi.HelmParameter
+		BeforeEach(func() {
+			goal = defaultValueFiles
+			actual = append(defaultValueFiles, "/values-excess.yaml")
+			goalHelm = []argoapi.HelmParameter{
+				{
+					Name:  "foo",
+					Value: "foovalue",
+				},
+				{
+					Name:        "bar",
+					Value:       "barvalue",
+					ForceString: true,
+				},
+				{
+					Name:        "baz",
+					Value:       "bazvalue",
+					ForceString: false,
+				},
+				{
+					Name:        "int1",
+					Value:       "1",
+					ForceString: false,
+				},
+				{
+					Name:        "int2",
+					Value:       "2",
+					ForceString: true,
+				},
+			}
+			actualHelm = append(goalHelm, argoapi.HelmParameter{
+				Name:  "excess",
+				Value: "excessvalue",
+			})
+		})
+
+		Context("Compare Helm Values", func() {
+			It("Compare different Helm Value Files", func() {
+				Expect(compareHelmValueFiles(goal, actual)).To(Equal(false))
+			})
+			It("Compare same Helm Value Files", func() {
+				sameGoal := goal
+				Expect(compareHelmValueFiles(goal, sameGoal)).To(Equal(true))
+			})
+		})
+
+		Context("Compare Helm Parameters", func() {
+			It("Compare different Helm Parameters", func() {
+				Expect(compareHelmParameters(goalHelm, actualHelm)).To(Equal(false))
+			})
+			It("Compare same Helm Parameters", func() {
+				sameGoalHelm := goalHelm
+				Expect(compareHelmParameters(goalHelm, sameGoalHelm)).To(Equal(true))
+			})
+			It("Test updateHelmParameter non existing Parameter", func() {
+				nonexistantParam := api.PatternParameter{
+					Name:  "Nonexistant",
+					Value: "nonexistantvalue",
+				}
+				Expect(updateHelmParameter(nonexistantParam, actualHelm)).To(Equal(false))
+			})
+			It("Test updateHelmParameter with existing Parameter with same value", func() {
+				existantParam := api.PatternParameter{
+					Name:  "foo",
+					Value: "foovalue",
+				}
+				Expect(updateHelmParameter(existantParam, actualHelm)).To(Equal(true))
+			})
+			It("Test updateHelmParameter with existing Parameter with different value", func() {
+				existantParam := api.PatternParameter{
+					Name:  "foo",
+					Value: "foovaluedifferent",
+				}
+				Expect(updateHelmParameter(existantParam, actualHelm)).To(Equal(true))
+			})
+
 		})
 	})
 })
