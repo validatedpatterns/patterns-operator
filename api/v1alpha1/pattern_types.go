3997770c4751a55f434383cbb5f9fbc3b3cf56b6
Michele Baldessari <michele@acksyn.org>
2023-09-03 13:02:20 +0200

    Add a PII not on the UUID field

--- api/v1alpha1/pattern_types.go
+++ api/v1alpha1/pattern_types.go
@@ -73,7 +73,7 @@ type PatternSpec struct {
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,order=7
 	ExtraValueFiles []string `json:"extraValueFiles,omitempty"`
 
-	// Analytics UUID. Leave empty to autogenerate a random one
+	// Analytics UUID. Leave empty to autogenerate a random one. Not PII information
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,order=9
 	AnalyticsUUID string `json:"analyticsUUID,omitempty"`
 	// Look for external changes every N minutes
