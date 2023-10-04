ee484036ef302c4b5ce179b2d6792f11fee95522
Lester Claudio <claudiol@redhat.com>
2023-10-04 11:43:24 -0600

    New common package for constants

---
+++ common/common.go
@@ -0,0 +1,8 @@
+package common
+
+const (
+	// Default Operator Namespace
+	OperatorNamespace = "openshift-operators"
+	// Default Operator Config Map File Name
+	OperatorConfigFile = "patterns-operator-config"
+)
