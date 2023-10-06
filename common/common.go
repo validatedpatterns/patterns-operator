71111a1f15b9c4c45583eebb455ca70ee2cbbc64
Lester Claudio <claudiol@redhat.com>
2023-10-06 09:17:55 -0600

    GitOps Config constants and PatternOperatorConfig map

--- common/common.go
+++ common/common.go
@@ -1,5 +1,7 @@
 package common
 
+// Below are the default constants that we will
+// use throughout the patterns operator code
 const (
 	// Default Operator Namespace
 	OperatorNamespace = "openshift-operators"
@@ -7,6 +9,7 @@ const (
 	OperatorConfigFile = "patterns-operator-config"
 )
 
+// GitOps Subscription
 const (
 	GitOpsDefaultChannel                = "gitops-1.8"
 	GitOpsDefaultPackageName            = "openshift-gitops-operator"
@@ -14,3 +17,15 @@ const (
 	GitOpsDefaultCatalogSourceNamespace = "openshift-marketplace"
 	GitOpsDefaultApprovalPlan           = "Automatic"
 )
+
+// GitOps Configuration
+const (
+	// Require manual intervention before Argo will sync new content. Default: False
+	GitOpsDefaultManualSync = "false"
+	// Require manual confirmation before installing and upgrading operators. Default: False
+	GitOpsDefaultManualApproval = "false"
+	// Dangerous. Force a specific version to be installed. Default: False
+	GitOpsDefaultUseCSV = "false"
+)
+
+var PatternsOperatorConfig map[string]string
