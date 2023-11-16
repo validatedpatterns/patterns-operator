616c1ecec7e2ae0b78495567c3f00896ea3fe9e9
Michele Baldessari <michele@acksyn.org>
2023-11-16 10:43:32 +0100

    Add a function to do helm templating on a string

--- controllers/values.go
+++ controllers/values.go
@@ -2,9 +2,14 @@ package controllers
 
 import (
 	"errors"
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"gopkg.in/yaml.v3"
+	"helm.sh/helm/v3/pkg/chart"
+	"helm.sh/helm/v3/pkg/chartutil"
+	"helm.sh/helm/v3/pkg/engine"
 )
 
 func mergeHelmValues(files ...string) (map[string]any, error) {
@@ -74,4 +79,59 @@ func getClusterGroupValue(key string, values map[string]any) any {
 		return v
 	}
 	return nil
+}
+
+func helmTpl(templateString string, valueFiles []string, values map[string]interface{}) (string, error) {
+	// Create a fake chart with the template.
+	fakeChart := &chart.Chart{
+		Metadata: &chart.Metadata{
+			APIVersion: "v2",
+			Name:       "fake",
+			Version:    "0.1.0",
+		},
+		Templates: []*chart.File{
+			{
+				Name: "templates/template.tpl",
+				Data: []byte(templateString),
+			},
+		},
+	}
+
+	// Load and merge values from the specified value files.
+	mergedValues := make(map[string]interface{})
+	for _, fileName := range valueFiles {
+		fileValues, err := chartutil.ReadValuesFile(filepath.Clean(fileName))
+		if err != nil {
+			return "", fmt.Errorf("error reading values file %s: %w", fileName, err)
+		}
+		mergedValues = chartutil.CoalesceTables(mergedValues, fileValues)
+	}
+
+	// Merge with the additional values provided.
+	mergedValues = chartutil.CoalesceTables(mergedValues, values)
+
+	// Render the template.
+	options := chartutil.ReleaseOptions{
+		Name:      "fake-release",
+		Namespace: "default",
+		IsInstall: true,
+		IsUpgrade: false,
+	}
+	valuesToRender, err := chartutil.ToRenderValues(fakeChart, mergedValues, options, chartutil.DefaultCapabilities)
+	if err != nil {
+		return "", fmt.Errorf("error preparing render values: %w", err)
+	}
+
+	renderedTemplates, err := engine.Render(fakeChart, valuesToRender)
+	if err != nil {
+		return "", fmt.Errorf("error rendering template: %w", err)
+	}
+
+	// Extract the rendered template.
+	rendered, ok := renderedTemplates["fake/templates/template.tpl"]
+	if !ok {
+		return "", fmt.Errorf("rendered template not found")
+	}
+
+	return rendered, nil
 }
