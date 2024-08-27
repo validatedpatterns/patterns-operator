928d7271e809b002abe44c15ecc1bae3e38f619c
Michele Baldessari <michele@acksyn.org>
2024-08-27 11:54:14 +0200

    Switch to gitops-1.13 by default
    It is supported all the way back to 4.12 at this point in time.

--- controllers/defaults.go
+++ controllers/defaults.go
@@ -17,7 +17,7 @@ const (
 
 // GitOps Subscription
 const (
-	GitOpsDefaultChannel                = "gitops-1.12"
+	GitOpsDefaultChannel                = "gitops-1.13"
 	GitOpsDefaultPackageName            = "openshift-gitops-operator"
 	GitOpsDefaultCatalogSource          = "redhat-operators"
 	GitOpsDefaultCatalogSourceNamespace = "openshift-marketplace"
