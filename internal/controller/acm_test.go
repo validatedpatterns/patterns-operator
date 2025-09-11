5384935dd12da98f5c89b9161f8df459c5ed8b01
Akos Eros <aeros@redhat.com>
2025-09-11 12:22:23 +0200

    WIP: drop dynamic client from reconcile object
    Refactor acm code, add tests, update vendor code

--- internal/controller/acm_test.go
+++ internal/controller/acm_test.go
@@ -4,16 +4,12 @@ import (
 	"context"
 	"fmt"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	dynamicfake "k8s.io/client-go/dynamic/fake"
-	"k8s.io/client-go/testing"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/client/interceptor"
 
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -21,58 +17,61 @@ import (
 )
 
 var _ = Describe("HaveACMHub", func() {
+
 	var (
 		patternReconciler *PatternReconciler
-		dynamicClient     *dynamicfake.FakeDynamicClient
-		gvrMCH            schema.GroupVersionResource
 		fakeClient        client.Client
+		configMap         *v1.ConfigMap
+		hub               *unstructured.Unstructured
 	)
-
 	BeforeEach(func() {
-		gvrMCH = schema.GroupVersionResource{Group: "operator.open-cluster-management.io", Version: "v1", Resource: "multiclusterhubs"}
-
-		dynamicClient = dynamicfake.NewSimpleDynamicClientWithCustomListKinds(runtime.NewScheme(), map[schema.GroupVersionResource]string{
-			gvrMCH: "MultiClusterHubList",
-		})
-
+		configMap = &v1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-configmap",
+				Namespace: "default",
+				Labels: map[string]string{
+					"ocm-configmap-type": "image-manifest",
+				},
+			},
+		}
+		hub = &unstructured.Unstructured{
+			Object: map[string]any{
+				"apiVersion": "operator.open-cluster-management.io/v1",
+				"kind":       "MultiClusterHub",
+				"metadata": map[string]any{
+					"name":      "test-hub",
+					"namespace": "default",
+				},
+			},
+		}
+		hub.SetGroupVersionKind(mchGVK)
 	})
 
-	Context("when the ACM Hub exists", func() {
-		BeforeEach(func() {
-			configMap := &v1.ConfigMap{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "test-configmap",
-					Namespace: "default",
-					Labels: map[string]string{
-						"ocm-configmap-type": "image-manifest",
-					},
-				},
-			}
-
-			fakeClient = fake.NewClientBuilder().WithScheme(testEnv.Scheme).
-				WithRuntimeObjects(configMap).Build()
-			patternReconciler = &PatternReconciler{
-				Client:        fakeClient,
-				dynamicClient: dynamicClient,
-			}
-
-			hub := &unstructured.Unstructured{
-				Object: map[string]any{
-					"apiVersion": "operator.open-cluster-management.io/v1",
-					"kind":       "MultiClusterHub",
-					"metadata": map[string]any{
-						"name":      "test-hub",
-						"namespace": "default",
-					},
-				},
-			}
-			_, err := dynamicClient.Resource(gvrMCH).Namespace("default").Create(context.Background(), hub, metav1.CreateOptions{})
-			Expect(err).ToNot(HaveOccurred())
-		})
-
+	Context("when the ACM Hub exists in same ns as configmap", func() {
 		It("should return true", func() {
+			fakeClient = fake.NewClientBuilder().WithScheme(testEnv.Scheme).
+				WithRuntimeObjects(configMap, hub).Build()
+			patternReconciler = &PatternReconciler{
+				Client: fakeClient,
+			}
+
 			result := haveACMHub(patternReconciler)
 			Expect(result).To(BeTrue())
+		})
+	})
+
+	Context("when the ACM Hub exists different ns as configmap", func() {
+		It("should return false", func() {
+			hub.SetNamespace("different")
+
+			fakeClient = fake.NewClientBuilder().WithScheme(testEnv.Scheme).
+				WithRuntimeObjects(configMap, hub).Build()
+			patternReconciler = &PatternReconciler{
+				Client: fakeClient,
+			}
+
+			result := haveACMHub(patternReconciler)
+			Expect(result).To(BeFalse())
 		})
 	})
 
@@ -81,8 +80,7 @@ var _ = Describe("HaveACMHub", func() {
 			fakeClient = fake.NewClientBuilder().WithScheme(testEnv.Scheme).
 				WithRuntimeObjects().Build()
 			patternReconciler = &PatternReconciler{
-				Client:        fakeClient,
-				dynamicClient: dynamicClient,
+				Client: fakeClient,
 			}
 
 			result := haveACMHub(patternReconciler)
@@ -91,47 +89,32 @@ var _ = Describe("HaveACMHub", func() {
 	})
 
 	Context("when there is an error listing ConfigMaps", func() {
-		BeforeEach(func() {
+		It("should return false and log the error", func() {
 			fakeClient = fake.NewClientBuilder().WithInterceptorFuncs(
 				interceptor.Funcs{List: func(ctx context.Context, client client.WithWatch, obj client.ObjectList, opts ...client.ListOption) error {
 					return fmt.Errorf("list error")
 				}}).WithScheme(testEnv.Scheme).Build()
-			patternReconciler = &PatternReconciler{
-				Client:        fakeClient,
-				dynamicClient: dynamicClient,
-			}
-		})
 
-		It("should return false and log the error", func() {
+			patternReconciler = &PatternReconciler{
+				Client: fakeClient,
+			}
+
 			result := haveACMHub(patternReconciler)
 			Expect(result).To(BeFalse())
 		})
 	})
 
 	Context("when there is an error listing the MultiClusterHubs", func() {
-		BeforeEach(func() {
-			configMap := &v1.ConfigMap{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "test-configmap",
-					Namespace: "default",
-					Labels: map[string]string{
-						"ocm-configmap-type": "image-manifest",
-					},
-				},
-			}
-			fakeClient = fake.NewClientBuilder().WithScheme(testEnv.Scheme).
-				WithRuntimeObjects(configMap).Build()
-
-			dynamicClient.PrependReactor("list", "multiclusterhubs", func(testing.Action) (handled bool, ret runtime.Object, err error) {
-				return true, nil, fmt.Errorf("multiclusterhub error")
-			})
-			patternReconciler = &PatternReconciler{
-				Client:        fakeClient,
-				dynamicClient: dynamicClient,
-			}
-		})
-
 		It("should return false and log the error", func() {
+			fakeClient = fake.NewClientBuilder().WithInterceptorFuncs(
+				interceptor.Funcs{List: func(ctx context.Context, client client.WithWatch, list client.ObjectList, opts ...client.ListOption) error {
+					return fmt.Errorf("list error")
+				}}).WithScheme(testEnv.Scheme).WithRuntimeObjects(configMap).Build()
+
+			patternReconciler = &PatternReconciler{
+				Client: fakeClient,
+			}
+
 			result := haveACMHub(patternReconciler)
 			Expect(result).To(BeFalse())
 		})
