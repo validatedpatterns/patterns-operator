fdbf69e8449e2ee07a2b25efad8cce28c0603590
Michele Baldessari <michele@acksyn.org>
2025-10-07 15:46:26 +0200

    Default to gitops-1.18

--- internal/controller/defaults.go
+++ internal/controller/defaults.go
@@ -17,7 +17,7 @@ const (
 
 // GitOps Subscription
 const (
-	GitOpsDefaultChannel                = "gitops-1.17"
+	GitOpsDefaultChannel                = "gitops-1.18"
 	GitOpsDefaultPackageName            = "openshift-gitops-operator"
 	GitOpsDefaultCatalogSource          = "redhat-operators"
 	GitOpsDefaultCatalogSourceNamespace = "openshift-marketplace"
