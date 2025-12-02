3e1f58e3d8d8c6aa2d048c6ce85cac41eeb07443
Akos Eros <aeros@redhat.com>
2025-12-02 16:57:12 +0100

    feat: Add rbac for pattern operator to create managedclusterviews
    This is needed for the pattern operator to be able to fetch the
    resources from spoke clusters via open-cluster-management search-api.

--- internal/controller/pattern_controller.go
+++ internal/controller/pattern_controller.go
@@ -94,6 +94,7 @@ type PatternReconciler struct {
 //+kubebuilder:rbac:groups=operator.openshift.io,resources="openshiftcontrollermanagers",resources=openshiftcontrollermanagers,verbs=get;list
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;create;update;watch
 //+kubebuilder:rbac:groups="route.openshift.io",namespace=vp-gitea,resources=routes;routes/custom-host,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="view.open-cluster-management.io",resources=managedclusterviews,verbs=create
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
