adfb770be421d3f352a722da650971b354dd6038
Michele Baldessari <michele@acksyn.org>
2026-03-24 11:42:06 +0100

    Add cors.conf in console test

--- console/scripts/catalog.sh
+++ console/scripts/catalog.sh
@@ -11,11 +11,16 @@ echo "Console Image: $UI_CATALOG_IMG"
 # Prefer podman if installed. Otherwise, fall back to docker.
 if [ -x "$(command -v podman)" ]; then
     podman run --pull always --platform $CONSOLE_IMAGE_PLATFORM --rm -p 8080:8080 \
-    --user 0 --entrypoint sh $UI_CATALOG_IMG -c "sed -i 's|root .*;|root /usr/share/nginx/html;|g' /etc/nginx/nginx.conf && nginx -g 'daemon off;'"
+    --user 0 --entrypoint sh $UI_CATALOG_IMG -c \
+    "sed -i 's|root .*;|root /usr/share/nginx/html;|g' /etc/nginx/nginx.conf && \
+     echo 'add_header Access-Control-Allow-Origin * always;' > /opt/app-root/etc/nginx.default.d/cors.conf && \
+     nginx -g 'daemon off;'"
 
 else
-    BRIDGE_PLUGINS="${PLUGIN_NAME}=http://host.docker.internal:9001"
     docker run --pull always --platform $CONSOLE_IMAGE_PLATFORM --rm -p 8080:8080 \
-    --user 0 --entrypoint sh $UI_CATALOG_IMG -c "sed -i 's|root .*;|root /usr/share/nginx/html;|g' /etc/nginx/nginx.conf && nginx -g 'daemon off;'"
+    --user 0 --entrypoint sh $UI_CATALOG_IMG -c \
+    "sed -i 's|root .*;|root /usr/share/nginx/html;|g' /etc/nginx/nginx.conf && \
+     echo 'add_header Access-Control-Allow-Origin * always;' > /opt/app-root/etc/nginx.default.d/cors.conf && \
+     nginx -g 'daemon off;'"
 
 fi
