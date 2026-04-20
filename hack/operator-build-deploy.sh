d472f546f3249c0b795fdc24dfd847c9af10176e
Giulio Cardillo <giulio.cardillo@gmail.com>
2026-04-20 12:37:04 +0200

    Revert CATALOGSOURCE="test-pattern-operator"

--- hack/operator-build-deploy.sh
+++ hack/operator-build-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -o pipefail
 
-CATALOGSOURCE="test-patterns-operator"
+CATALOGSOURCE="test-pattern-operator"
 DEFAULT_NS="patterns-operator"
 OPERATOR="patterns-operator"
 VERSION="${VERSION:-6.6.6}"
